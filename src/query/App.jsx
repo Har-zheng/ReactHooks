b2579bca4df8fd0c2b7b6f436ea9497b8ec54238
zhz <1456421562@qq.com>
2020-04-28 14:48:06 +0800

    feat

--- src/query/App.jsx
+++ src/query/App.jsx
@@ -1,15 +1,17 @@
 import React from "react";
 import { connect } from "react-redux";
+import Header from "../common/Header";
 import Nav from "../common/Nav";
 import List from "./List";
 import Bottom from "./Bottom";
+
 // import { ACTION_SET_FROM } from "./actions";
 
 import "./App.css";
 
 function App(props) {
   return (
-      <div>
+      <div className="header-wrapper">
           <Nav />
           <List />
           <Bottom />
